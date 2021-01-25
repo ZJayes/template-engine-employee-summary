0c1af7fa11cbdfdd6077ab8d1efccd7e11744ae3
Zachary Jayes <zacjayes@gmail.com>
2021-01-24 18:54:41 -0600

    push for the night

--- Develop/app.js
+++ Develop/app.js
@@ -11,9 +11,16 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 
+const teamMembers = () => {
+    inquirer
+        .prompt([
+            
+        ])
+}
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
